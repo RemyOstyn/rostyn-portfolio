aef5c20de72afce5b8b38fb36228a9b208640a09
RemyOstyn <ostyn.r@gmail.com>
2025-02-25 23:52:55 +0700

    fix: Remove unused import

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import { Metadata, ResolvingMetadata } from "next";
+import { Metadata } from "next";
 import { projects } from "../../data/projects";
 import ProjectDisplay from "@/app/components/ProjectDisplay";
 
